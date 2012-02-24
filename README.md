afe2920a0ea739214e702c7adccc4e7c49517a83
Henry Jobst <github@jobst-berlin.de>
2012-02-24 22:38:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Race-Manager
+
+by [Henry Jobst]{http://www.jobst-berlin.de/~henry/}
+
+This will be a web application to manage and present results of running races.
+At first development will start with orientieering competitions for team events with data input from SI-hardware stations.
+
+For further information on this software please contact: race_manager-development@jobst-berlin.de
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaceManager::Application.config.secret_token = 'db360d10d68a462b84ca24cfdd82c3e60fdde3946550adcd66d5319a59137ae75c9967ecece9ee37bc4c3b5f1f2f81b25843cfc8068ee5e218da9bd653215f50'

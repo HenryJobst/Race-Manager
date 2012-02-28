7ef5e3139f5042bcf555bfb42cc1a344a1a06b6b
Henry Jobst <github@jobst-berlin.de>
2012-02-28 23:13:04 +0100

    add associations

--- app/models/lcevent.rb
+++ app/models/lcevent.rb
@@ -1,2 +1,5 @@
 class Lcevent < ActiveRecord::Base
+set_primary_key "id_event"
+has_many :stamps
+
 end
--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -1,2 +1,5 @@
 class Stamp < ActiveRecord::Base
+set_primary_key "id_stamp"
+belongs_to :lcevent, :foreign_key => "id_event"
+
 end

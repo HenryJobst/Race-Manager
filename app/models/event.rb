7cde42c538fc5be9821a39c1491379cb76853060
Henry Jobst <github@jobst-berlin.de>
2012-03-07 23:41:13 +0100

    enhance team pages and correct errors in competitor pages

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ActiveRecord::Base
   set_table_name "lcevents" 
   set_primary_key "id_event"
-  has_many :subevents
+  has_many :subevents, :primary_key => "event_foreign_id"
   has_many :stamps, :primary_key => "event_foreign_id", :foreign_key => "id_event"
-  has_many :teams
-  has_many :event_competitors
-  has_many :competitors, :through => :event_competitors
+  has_many :teams, :primary_key => "event_foreign_id"
+  has_many :event_competitors, :primary_key => "event_foreign_id"
+  has_many :competitors, :through => :event_competitors, :primary_key => "event_foreign_id"
 end
